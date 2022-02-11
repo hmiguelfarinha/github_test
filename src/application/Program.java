ef3df81368631eb22c8358f93b45086e21d778b6
Hugo Farinha <hugo.m.farinha@gmail.com>
2022-02-11 09:25:29 +0000

    Escrito Bom dia

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,15 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		System.out.println("Bom dia");
+		System.out.println("Bom dia");
+		System.out.println("Bom dia");
+		System.out.println("Bom dia");
+		System.out.println("Bom dia");
+		System.out.println("Bom dia");
+		System.out.println("Bom dia");
+		System.out.println("Bom dia");
+		System.out.println("Bom dia");
 		
 
 	}
